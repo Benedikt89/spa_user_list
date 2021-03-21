63dab07165dead5725d6d609451f431b05be88aa
Benedikt89 <constantine.kostka@gmail.com>
2021-03-21 12:54:12 +0100

    added phone validation

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,11 +1,10 @@
 import {I_user} from "../types/user-types";
 import * as React from "react";
-import {Button, Form, Input} from 'antd';
+import {Button, Form, Input, Select} from 'antd';
 import './common/common.css';
 import {newUserId} from "../redux/users/reducer";
 import {useDispatch, useSelector} from "react-redux";
 import {onUserUpdate} from "../redux/users/actions";
-import {useState} from "react";
 import {selectFetchingByKey} from "../redux/app/selectors";
 import {AppStateType} from "../redux/store";
 
@@ -19,22 +18,47 @@ const UserForm: React.FC<UserFormProps> = ({user}) => {
     loading: selectFetchingByKey(state, 'user')
   }));
 
+  const phoneRegExp = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  const defaultPrefix = "86";
+
   const onFinish = (values: any) => {
     dispatch(onUserUpdate({
+      ...values,
       id: user ? user.id : newUserId,
-      ...values
+      phone: `+${values.prefix ? values.prefix : defaultPrefix}-${values.phone}`,
     }));
     console.log('Success:', values);
   };
 
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select style={{ width: 70 }} defaultValue={defaultPrefix}>
+        <Select.Option value="86">+86</Select.Option>
+        <Select.Option value="87">+87</Select.Option>
+      </Select>
+    </Form.Item>
+  );
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
   };
+  let getSplittedPhone = (string: string) => {
+    let [prefix, ...rest] = string.split("-");
+    let validPrefix = prefix.length === 3;
+    if (!validPrefix) {
+      rest.unshift(prefix)
+    }
+    return {prefix: validPrefix ? prefix : defaultPrefix, phone: rest.join('')};
+  };
+
+  let defaults = user ? {
+    ...user,
+    ...getSplittedPhone(user.phone)
+  } : {};
   return (
     <Form
       className="row"
       name="user"
-      initialValues={user ? user : {}}
+      initialValues={defaults}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
     >
@@ -47,9 +71,13 @@ const UserForm: React.FC<UserFormProps> = ({user}) => {
 
       <Form.Item
         name="phone"
-        rules={[{required: true, message: 'Please input your phone!'}]}
+        rules={[
+          {required: true, message: 'Please input your phone!'},
+          {pattern: phoneRegExp,
+            message: 'Please enter a valid Phone *** *** ****',}
+          ]}
       >
-        <Input placeholder="Phone"/>
+        <Input addonBefore={prefixSelector} style={{ width: '100%' }} placeholder="Phone" />
       </Form.Item>
 
       <Form.Item>
